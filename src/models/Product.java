2d460c97cebfb521443ac2c5870e37e5829f9776
Ashraf22 <ashrafmaged593@gmail.com>
2024-11-03 17:38:08 +0200

    [add] weight variable

--- src/models/Product.java
+++ src/models/Product.java
@@ -4,12 +4,14 @@ public class Product {
     private String name;
     private double price;
     private int quantity;
+    private double weight;
     private boolean isExpirable;
     private boolean requiresShipping;
-    public Product(String name, double price, int quantity, boolean isExpirable, boolean requiresShipping) {
+    public Product(String name, double price, int quantity, double weight,boolean isExpirable, boolean requiresShipping) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+        this.weight = weight;
         this.isExpirable = isExpirable;
         this.requiresShipping = requiresShipping;
     }
@@ -26,13 +28,13 @@ public class Product {
         return quantity;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setPrice(double price) {
+//        this.price = price;
+//    }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
