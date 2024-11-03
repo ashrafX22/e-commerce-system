ae94df489eb3baa6b16f694ab4eabd2ed68d0447
Ashraf22 <ashrafmaged593@gmail.com>
2024-11-03 14:18:55 +0200

    add procucts model

---
+++ src/products.java
@@ -0,0 +1,38 @@
+public class products {
+    private String name;
+    private double price;
+    private int quantity;
+    private boolean isExpirable;
+    private boolean requiresShipping;
+    public products(String name, double price, int quantity,boolean isExpirable,boolean requiresShipping) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.isExpirable = isExpirable;
+        this.requiresShipping = requiresShipping;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setPrice(double price) {
+//        this.price = price;
+//    }
+//
+//    public void setQuantity(int quantity) {
+//        this.quantity = quantity;
+//    }
+}
