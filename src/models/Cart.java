a559bf956488d0b2353ff45253ac9db890013064
Ashraf22 <ashrafmaged593@gmail.com>
2024-11-03 18:50:49 +0200

    [fix] products vector outside of the add function

--- src/models/Cart.java
+++ src/models/Cart.java
@@ -6,14 +6,19 @@ import errors.InsufficientQuantityException;
 import java.util.Vector;
 
 public class Cart {
-    private Vector<CartItem> items;
+    private Vector<CartItem> items = new Vector<CartItem>();
     private Customer customer;
 
     public Vector<CartItem> getItems() {
         return items;
     }
 
-    public boolean add(Product item, int amount) {
+
+    public Cart(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void add(Product item, int amount) {
         try {
             if (item.getQuantity() < amount) {
                 throw new InsufficientQuantityException("Insufficient quantity of product: " + item.getName());
@@ -22,16 +27,12 @@ public class Cart {
             if (item.getPrice() * amount > this.customer.getBalance()) {
                 throw new InsufficientBalanceException("Insufficient balance for product: " + item.getName());
             }
-
             this.items.add(new CartItem(item, amount));
+
             item.setQuantity(item.getQuantity() - amount);
             this.customer.updateBalance((int) (item.getPrice() * amount));
         } catch (InsufficientQuantityException | InsufficientBalanceException e) {
             System.out.println(e.getMessage());
-            return false;
         }
-
-
-        return true;
     }
 }
