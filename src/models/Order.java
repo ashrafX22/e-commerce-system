46dfa93177972c5fe9ddcdf69e25ea52b5f31817
Ashraf22 <ashrafmaged593@gmail.com>
2024-11-03 18:52:14 +0200

    [add] list of items,constructor,remove setters

--- src/models/Order.java
+++ src/models/Order.java
@@ -1,16 +1,16 @@
 package models;
 
+import java.util.Vector;
+
 public class Order {
     private double subtotal;
     private double tax;
     private double total;
     private double totalWeight;
+    private Vector<CartItem> items;
 
-    public Order(double subtotal, double tax, double total, double totalWeight) {
-        this.subtotal = subtotal;
-        this.tax = tax;
-        this.total = total;
-        this.totalWeight = totalWeight;
+    public Order(Vector<CartItem> items) {
+        this.items = items;
     }
 
     public double getSubtotal() {
@@ -25,23 +25,52 @@ public class Order {
         return tax;
     }
 
-    public void setTax(double tax) {
-        this.tax = tax;
-    }
+
 
     public double getTotal() {
         return total;
     }
 
-    public void setTotal(double total) {
-        this.total = total;
-    }
+
 
     public double getTotalWeight() {
         return totalWeight;
     }
 
-    public void setTotalWeight(double totalWeight) {
-        this.totalWeight = totalWeight;
+
+    public void calculateSubtotal() {
+            for (CartItem item : items) {
+               this.subtotal += item.getProduct().getPrice() * item.getQuantity();
+            }
     }
+    public void calculateTotalWeight() {
+        for (CartItem item : items) {
+            this.totalWeight += item.getProduct().getWeight() * item.getQuantity();
+        }
+    }
+    public void calculateTotal() {
+        this.total = this.subtotal + this.tax;
+    }
+
+    public void calculateTax(double taxRate) {
+        this.tax = subtotal * taxRate;
+    }
+
+    public void printReceipt(){
+        calculateSubtotal();
+        calculateTotalWeight();
+        calculateTotal();
+        calculateTax(0.08); // Example tax rate
+        System.out.println("Order Receipt:");
+        System.out.println("Subtotal: " + this.subtotal);
+        System.out.println("Tax: " + this.tax);
+        System.out.println("Total: " + this.total);
+        System.out.println("Total Weight: " + this.totalWeight);
+        System.out.println("--------------------------------------");
+        for (CartItem item : items) {
+            System.out.println(item.getProduct().getName() + ": " + item.getQuantity() + "x " + item.getProduct().getPrice());
+        }
+        System.out.println("--------------------------------------");
+    }
+
 }
